db4cb03fdee4143a79f2911bc3f76af66158bae8
Adetunji Adebowale <bowaleadetunji@gmail.com>
2023-08-20 20:12:44 +0100

    added js implementation of the task one

---
+++ other_implementation/TimeDuration.js
@@ -0,0 +1,14 @@
+// dommy TimeDuration class in js
+class TimeDuration {
+    constructor (pointA = [], pointB = []) {
+        pointA = pointA,
+        pointB = pointB;
+    };
+    
+    duration() {
+        //dummy return
+        return 120;
+    }
+};
+
+export default TimeDuration;
