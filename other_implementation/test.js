5005331680f2ea0371ae9e90f04bcf2a34fff89a
Adetunji Adebowale <66047485+KoolTeck@users.noreply.github.com>
2023-09-21 08:58:39 +0100

    Delete other_implementation directory

--- other_implementation/test.js
+++
@@ -1,8 +0,0 @@
-// testing the module
-import CheckObstruction from "./check_route.js";
-import CascadingPalindrome from "./cascadingPalindrome.js";
-const routeOne = new CheckObstruction([130.3, 133.4], [100, 120], 50);
-// console.log(routeOne.checkRoute());
-// console.log("-----------------------");
-// const routeTwo = new CheckObstruction([53.5872, 2.4138], [53.5872, 2.4138], 15);
-// console.log(routeTwo.checkRoute());
